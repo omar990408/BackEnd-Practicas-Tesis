8879212c354bca37d6c43909563a229a62689ae1
Omar González <omar_gonza8@live.com>
2023-04-16 19:29:59 -0500

    35%

--- src/main/java/com/sistema/examenes/controller/UsuarioController.java
+++ src/main/java/com/sistema/examenes/controller/UsuarioController.java
@@ -25,6 +25,7 @@ public class UsuarioController {
         Rol rol = new Rol();
         rol.setRolId(2L);
         rol.setNombre("NORMAL");
+
         UsuarioRol usuarioRol = new UsuarioRol();
         usuarioRol.setUsuario(usuario);
         usuarioRol.setRol(rol);
